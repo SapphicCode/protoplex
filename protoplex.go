ac157b0a1bfdfa879e58ca3d452df4fba8ab24fb
Pandentia <hi@pandentia.qcx.io>
2019-01-11 14:40:19 +0100

    Run gofmt and sort by protocol efficiency

--- protoplex.go
+++ protoplex.go
@@ -12,8 +12,8 @@ func main() {
 	tls := flag.String("tls", "", "The TLS/HTTPS server address")
 	openvpn := flag.String("ovpn", "", "The OpenVPN server address")
 	http := flag.String("http", "", "The HTTP server address")
-        socks5 := flag.String("socks5", "", "The SOCKS5 server address")
-        socks4 := flag.String("socks4", "", "The SOCKS4 server address")
+	socks5 := flag.String("socks5", "", "The SOCKS5 server address")
+	socks4 := flag.String("socks4", "", "The SOCKS4 server address")
 
 	flag.Parse()
 
@@ -30,12 +30,12 @@ func main() {
 	if *http != "" {
 		p = append(p, protocols.NewHTTPProtocol(*http))
 	}
-        if *socks5 != "" {
-                p = append(p, protocols.NewSOCKS5Protocol(*socks5))
-        }
-        if *socks4 != "" {
-                p = append(p, protocols.NewSOCKS4Protocol(*socks4))
-        }
+	if *socks5 != "" {
+		p = append(p, protocols.NewSOCKS5Protocol(*socks5))
+	}
+	if *socks4 != "" {
+		p = append(p, protocols.NewSOCKS4Protocol(*socks4))
+	}
 
 	protoplex.RunServer(*bind, p)
 }
