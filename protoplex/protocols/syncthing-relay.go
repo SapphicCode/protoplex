38e5582890b5a2112125d9545951f68d58e0ca6b
Cassandra <hi@pandentia.qcx.io>
2019-01-15 14:44:28 +0100

    Improve Syncthing Relay matching

--- protoplex/protocols/syncthing-relay.go
+++ protoplex/protocols/syncthing-relay.go
@@ -2,8 +2,11 @@ package protocols
 
 func NewSTRelayProtocol(targetAddress string) *Protocol {
 	return &Protocol{
-		Name:                    "STRelay",
-		Target:                  targetAddress,
-		MatchBytes:              [][]byte{{'b', 'e', 'p', '-', 'r', 'e', 'l', 'a', 'y'}},
+		Name:   "STRelay",
+		Target: targetAddress,
+		MatchBytes: [][]byte{
+			{'b', 'e', 'p', '-', 'r', 'e', 'l', 'a', 'y'},
+			{'b', 'e', 'p', '/'},
+		},
 	}
 }
