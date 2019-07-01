d02ab5306a5a2db244408cc907cc1614883b4bb4
Cassandra <cassandra@beelen.one>
2019-07-01 12:46:02 +0200

    Make OpenVPN matching more broad
    This is an expensive change, so OpenVPN has been moved down in the protocol chain.
    
    Fixes #5.

--- protoplex/protocols/openvpn.go
+++ protoplex/protocols/openvpn.go
@@ -1,9 +1,15 @@
 package protocols
 
+import "regexp"
+
 func NewOpenVPNProtocol(targetAddress string) *Protocol {
 	return &Protocol{
-		Name:            "OpenVPN",
-		Target:          targetAddress,
-		MatchStartBytes: [][]byte{{0x00, 0x0e, 0x38}},
+		Name:   "OpenVPN",
+		Target: targetAddress,
+		// MatchStartBytes: [][]byte{{0x00, 0x0e, 0x38}},
+		MatchRegexes: []*regexp.Regexp{
+			regexp.MustCompile("^\x00\x0D-\xFF\x38"), // asumming this variant is more common in newer clients
+			regexp.MustCompile("^\x00\x0D-\xFF$"),
+		},
 	}
 }
