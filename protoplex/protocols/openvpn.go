8f634e62a09d7291a8f671adcbcccb66bff49459
Cassandra <cassandra@beelen.one>
2019-07-01 13:32:05 +0200

    Fix the UTF-8 panic

--- protoplex/protocols/openvpn.go
+++ protoplex/protocols/openvpn.go
@@ -8,8 +8,8 @@ func NewOpenVPNProtocol(targetAddress string) *Protocol {
 		Target: targetAddress,
 		// MatchStartBytes: [][]byte{{0x00, 0x0e, 0x38}},
 		MatchRegexes: []*regexp.Regexp{
-			regexp.MustCompile("^\x00\x0D-\xFF\x38"), // asumming this variant is more common in newer clients
-			regexp.MustCompile("^\x00\x0D-\xFF$"),
+			regexp.MustCompile(`^\x00[\x0d-\xff]\x38`), // asumming this variant is more common in newer clients
+			regexp.MustCompile(`^\x00[\x0d-\xff]$`),
 		},
 	}
 }
